791179e64f19d75b4144e41815e2d8a6f6090f99
5Hanui <gkdud644@naver.com>
2023-11-25 05:19:08 +0900

    feat: build.gradle.kts script 수정

--- pokemon/build.gradle.kts
+++ pokemon/build.gradle.kts
@@ -24,6 +24,7 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.8.1")
     implementation("com.squareup.retrofit2:adapter-rxjava:2.8.1")
     implementation("com.github.ajalt.mordant:mordant:2.2.0")
+    implementation("net.java.dev.jna:jna:5.13.0")
 }
 
 tasks.withType<KotlinCompile> {
